b136b4437e9631307880ba8d85a60efc9cdbbb29
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-05 01:24:14 +0500

    preparing setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div>
+        <h1>Home page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit mollitia
+          blanditiis, perferendis ducimus iure assumenda. Ex quam, dolore atque
+          amet culpa nihil hic quidem ipsam?
+        </p>
+      </div>
+    </Layout>
+  )
 }
